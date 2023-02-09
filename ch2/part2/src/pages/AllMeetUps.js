8adc7655c0d688967d0176b84fe4546a9e0871d8
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-09 22:05:57 +0545

    getting started with fetching data

--- ch2/part2/src/pages/AllMeetUps.js
+++ ch2/part2/src/pages/AllMeetUps.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MeetupList from '../components/meetups/MeetupList';
 
 function AllMeetUps() {
+
+    const [isLoading,setIsLoading] = useState(true);
+    const [loadedMeetups,setLoadedMeetups] = useState([]);
+
+    fetch(
+        'https://react-getting-started-122ec-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json'
+    ).then( response => {
+        return response.json();
+    }).then( data => {
+        setIsLoading(false);
+        setLoadedMeetups(data);
+    });
+
+    if(isLoading){
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        )
+    }
 
     const DUMMY_DATA = [
         {
