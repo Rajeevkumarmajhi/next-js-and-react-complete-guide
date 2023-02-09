a620b1c13a659247da9195302a13fd3df13857e9
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-09 22:20:17 +0545

    using the useEffect hook

--- ch2/part2/src/pages/AllMeetUps.js
+++ ch2/part2/src/pages/AllMeetUps.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import MeetupList from '../components/meetups/MeetupList';
 
 function AllMeetUps() {
@@ -6,14 +6,26 @@ function AllMeetUps() {
     const [isLoading,setIsLoading] = useState(true);
     const [loadedMeetups,setLoadedMeetups] = useState([]);
 
-    fetch(
-        'https://react-getting-started-122ec-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json'
-    ).then( response => {
-        return response.json();
-    }).then( data => {
-        setIsLoading(false);
-        setLoadedMeetups(data);
-    });
+    useEffect(()=>{
+        setIsLoading(true);
+        fetch(
+            'https://react-getting-started-122ec-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json'
+        ).then( response => {
+            return response.json();
+        }).then( data => {
+            const meetups = [];
+            for(const key in data){
+                const meetup = {
+                    id:key,
+                    ...data[key]
+                }
+                meetups.push(meetup)
+            }
+            setIsLoading(false);
+            setLoadedMeetups(meetups);
+        });
+
+    },[]);
 
     if(isLoading){
         return (
@@ -23,31 +35,10 @@ function AllMeetUps() {
         )
     }
 
-    const DUMMY_DATA = [
-        {
-            id: 'm1',
-            title: 'This is a first meetup',
-            image:
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-            address: 'Meetupstreet 5, 12345 Meetup City',
-            description:
-                'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-        },
-        {
-            id: 'm2',
-            title: 'This is a second meetup',
-            image:
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-            address: 'Meetupstreet 5, 12345 Meetup City',
-            description:
-                'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-        },
-    ];
-
     return (
         <div>
             <h1>All meetup page</h1>
-            <MeetupList meetups={DUMMY_DATA} />
+            <MeetupList meetups={loadedMeetups} />
 
         </div>
     )
