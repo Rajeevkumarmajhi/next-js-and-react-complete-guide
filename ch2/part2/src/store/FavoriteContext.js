32ffc2bd8447637839b2d57fff64d8b09559c87f
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-09 22:52:37 +0545

    introduction to React Context

---
+++ ch2/part2/src/store/FavoriteContext.js
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react"
+
+const FavoriteContext = createContext({
+    favorites: [],
+    totalFavorites: 0,
+});
+
+function FavoriteContextProvider(props) {
+    const [userFavorites, setUserFavorites] = useState([]);
+
+    const context = {
+        favorites:userFavorites,
+        totalFavorites:userFavorites.length
+    };
+
+
+    return <FavoriteContext.Provider value={context}>
+        { props.children}
+    </FavoriteContext.Provider>
+}
+
+export default FavoriteContext
