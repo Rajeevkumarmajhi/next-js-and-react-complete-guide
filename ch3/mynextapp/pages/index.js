5ca501dbcfa6f4c76fc1c86c4f68a5ddb7888faf
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-10 23:35:49 +0545

    navigating with the link components

--- ch3/mynextapp/pages/index.js
+++ ch3/mynextapp/pages/index.js
@@ -1,9 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
 
 function HomePage() {
   return (
     <div>
       <h1>This is home page. good</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio" >Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   )
 }
