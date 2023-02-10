fefd6c023a80d5c80f9c3244f4a8a8b3f6583c6f
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-10 22:13:50 +0545

    extracting dynamic path segment

--- ch3/mynextapp/pages/portfolio/[projectid].js
+++ ch3/mynextapp/pages/portfolio/[projectid].js
@@ -1,6 +1,11 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 function PortFolioProjectPage() {
+    const router = useRouter();
+
+    console.log(router.pathname);
+    console.log(router.query);
     return (
         <div>
             <h1>Portfolio Project Page</h1>
