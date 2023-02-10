88a9a21cb1f202d5ec7baa3eee27c088ed897e3e
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-10 20:17:20 +0545

    Adding dynamic routing and path

---
+++ ch3/mynextapp/pages/portfolio/[projectid].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PortFolioProjectPage() {
+    return (
+        <div>
+            <h1>Portfolio Project Page</h1>
+        </div>
+    )
+}
+
+export default PortFolioProjectPage
