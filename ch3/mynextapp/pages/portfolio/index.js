fc1274dffca6531e0293047dc1997390400d8a9e
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-10 20:11:20 +0545

    working with the nested paths and routes

---
+++ ch3/mynextapp/pages/portfolio/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+    return (
+        <div>
+            <h1>This is good start</h1>
+        </div>
+    )
+}
+
+export default index
---
+++ ch3/mynextapp/pages/portfolio/list.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function list() {
+    return (
+        <div>
+            <h1>This is list page</h1>
+        </div>
+    )
+}
+
+export default list
