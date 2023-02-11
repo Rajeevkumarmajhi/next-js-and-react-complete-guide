89fbac6e698c91b21f20f573057a2b69b2a94be7
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-11 19:18:51 +0545

    Navigating to dynamic routes

--- ch3/mynextapp/pages/clients/index.js
+++ ch3/mynextapp/pages/clients/index.js
@@ -1,9 +1,22 @@
+import Link from 'next/link';
 import React from 'react'
 
 function clients() {
+
+    const clients = [
+        { id: "max",name:"Rajeev Majhi"},
+        { id: "manu",name:"Manueal Majhi"},
+    ];
+
+
     return (
         <div>
             <h1>Clients page</h1>
+            <ul>
+                { clients.map( client => <li key={client.id}>
+                    <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                </li> )}
+            </ul>
         </div>
     )
 }
