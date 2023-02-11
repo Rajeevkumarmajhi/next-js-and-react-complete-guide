178cde81225611e6ce3cf3b0fdf87b205f4a0f4c
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-11 22:19:59 +0545

    setting up the main pages

---
+++ ch4/nextevents/pages/events/[...slug].jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FilteredEventPage() {
+    return (
+        <div>
+            <h1>Filtered Page</h1>
+        </div>
+    )
+}
+
+export default FilteredEventPage
