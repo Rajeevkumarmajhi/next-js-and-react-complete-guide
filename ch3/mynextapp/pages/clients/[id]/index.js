4ac6ed4840f393ac08105e46cc91904b489c0c66
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-11 21:00:29 +0545

    navigating programming way

--- ch3/mynextapp/pages/clients/[id]/index.js
+++ ch3/mynextapp/pages/clients/[id]/index.js
@@ -1,15 +1,29 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 
-function index() {
+function ClientProjectPage() {
     const router = useRouter();
-    console.log(router.query);
+    // console.log(router.query);
+
+    function loadProjectHandler(){
+        router.push({
+            pathname:'/clients/[id]/[clientprojectid]',
+            query:{
+                id:"max",
+                clientprojectid:'projecta',
+            }
+        });
+    }
+
+
     return (
         <div>
-            This is selected client page
+            <h1>The project of a given client</h1>\
+            <button onClick={loadProjectHandler}>Load Project A</button>
+
 
         </div>
     )
 }
 
-export default index
+export default ClientProjectPage
