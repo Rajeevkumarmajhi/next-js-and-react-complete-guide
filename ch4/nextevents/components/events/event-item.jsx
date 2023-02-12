50a11dcd8bdeead696625ed8be605e60d0383bcd
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-12 20:44:35 +0545

    Adding buttons and icons

--- ch4/nextevents/components/events/event-item.jsx
+++ ch4/nextevents/components/events/event-item.jsx
@@ -1,4 +1,7 @@
-import Link from 'next/link';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import DateIcon from '../icons/date-icon';
+import Button from '../ui/button';
 import classes from './event-item.module.css';
 
 function EventItem(props) {
@@ -23,14 +26,18 @@ function EventItem(props) {
                     <h2>{title}</h2>
                 </div>
                 <div className={classes.date}>
-                    <time>{ humanReadableDate }</time>
+                    <time> <DateIcon /> { humanReadableDate }</time>
                 </div>
                 <div className={classes.address}>
+                    <AddressIcon />
                     <address>{ formattedAddress }</address>
                 </div>
             </div>
             <div className={classes.actions}>
-                <Link href={exploredLink} > Explore Events</Link>
+                <Button link={exploredLink}>
+                    <span>Explore Events</span>
+                    <span className={classes.icon}><ArrowRightIcon /></span>
+                </Button>
             </div>
             
         </li>
