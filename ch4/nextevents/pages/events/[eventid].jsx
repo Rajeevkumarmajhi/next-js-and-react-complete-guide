98becf046f0ed4422145fb88547c7d8137f22aa8
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-12 21:51:13 +0545

    Adding the event detail page

--- ch4/nextevents/pages/events/[eventid].jsx
+++ ch4/nextevents/pages/events/[eventid].jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import EventContent from '@/components/event-detail/event-content';
+import EventLogistics from '@/components/event-detail/event-logistics';
+import EventSummary from '@/components/event-detail/event-summary';
+import { getEventById } from '@/dummy-data';
+import { useRouter } from 'next/router'
+import React, { Fragment } from 'react'
 
 function eventDetailPage() {
+    const router = useRouter();
+
+    const eventId = router.query.eventid;
+    const event = getEventById(eventId);
+
+    if(!event)  {
+        return <p>No event found!</p>
+    }
     return (
         <div>
-            <h1>Event Detail</h1>
+            <Fragment>
+                <EventSummary title={event.title} />
+                <EventLogistics date={event.date} address={event.location} image={event.image}  imageAlt={event.title} />
+                <EventContent>
+                    <p> { event.description } </p>
+                </EventContent>
+            </Fragment>
         </div>
     )
 }
