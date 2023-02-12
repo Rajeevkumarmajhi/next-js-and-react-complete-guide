9ad097fd6ddeff155a2dc1bbe60be124921b1235
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-12 22:48:52 +0545

    working on all events page

--- ch4/nextevents/pages/events/index.jsx
+++ ch4/nextevents/pages/events/index.jsx
@@ -1,9 +1,12 @@
+import EventList from '@/components/events/event-list';
+import { getAllEvents } from '@/dummy-data'
 import React from 'react'
 
 function AllEventPage() {
+    const events = getAllEvents();
     return (
         <div>
-            <h1>All Events</h1>
+            <EventList items={events}/>
         </div>
     )
 }
