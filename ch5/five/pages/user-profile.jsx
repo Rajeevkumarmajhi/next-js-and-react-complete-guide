03b3ca06e28aa42b05130a545ab69810867ea9ec
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-02-20 23:11:58 +0545

    implementing client side data fetching

--- ch5/five/pages/user-profile.jsx
+++ ch5/five/pages/user-profile.jsx
@@ -10,7 +10,7 @@ export default UserProfilePage
 
 export async function getServerSideProps(context){
     const {params,req,res } = context;
-    
+
     return {
         props:{
             username:"Max"
