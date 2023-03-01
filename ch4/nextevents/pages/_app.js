7f5716ca630dda2fee3025795d65c8650c03841c
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-03-01 23:10:08 +0545

    adding global title in _app.js which can be overwritten by child components

--- ch4/nextevents/pages/_app.js
+++ ch4/nextevents/pages/_app.js
@@ -5,6 +5,7 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return <Layout>
     <Head>
+      <title>Rajeev Kumar Majhi (Events App)</title>
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
     <Component {...pageProps} />
