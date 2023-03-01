f2c4613d762c403616f909c7733d124b13f68c98
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-03-01 22:39:44 +0545

    adding head content and title in page

--- ch4/nextevents/pages/index.js
+++ ch4/nextevents/pages/index.js
@@ -1,10 +1,16 @@
 import EventList from "@/components/events/event-list";
 import { getFeaturedEvents } from "@/helpers/api-util";
+import Head from "next/head";
 
 export default function HomePage(props) {
 
+
   return (
     <div>
+        <Head>
+          <title>Next Js Events By Rajeev Majhi</title>
+          <meta name="description" content="Find a lot of great events by rajeev kumar majhi that allows you to evolve..." />
+        </Head>
         <EventList items={props.events} />
     </div>
   )
