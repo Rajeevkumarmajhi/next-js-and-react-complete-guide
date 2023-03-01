8a2204795267b56214a2a8efa168ff897403fffe
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-03-01 20:26:31 +0545

    working on all events page server side fetching

--- ch4/nextevents/pages/events/index.jsx
+++ ch4/nextevents/pages/events/index.jsx
@@ -1,12 +1,12 @@
 import EventList from '@/components/events/event-list';
 import EventSearch from '@/components/events/events-search';
-import { getAllEvents } from '@/dummy-data'
+import { getAllEvents } from '@/helpers/api-util';
 import { useRouter } from 'next/router';
 import React, { Fragment } from 'react'
 
-function AllEventPage() {
+function AllEventPage(props) {
     const router = useRouter();
-    const events = getAllEvents();
+    const {events} = props;
 
     function findEventHandler(year,month)
     {
@@ -21,6 +21,17 @@ function AllEventPage() {
             <EventList items={events}/>
         </Fragment>
     )
+}
+
+export async function getStaticProps(){
+    const events = await getAllEvents();
+
+    return {
+        props:{
+            events: events,
+        },
+        revalidate:60,
+    }
 }
 
 export default AllEventPage
