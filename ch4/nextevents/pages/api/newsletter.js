d7c6a238dee66204722c704d07ed06f7b6d0a67f
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-03-03 23:22:46 +0545

    running mongo db queries using api routes

--- ch4/nextevents/pages/api/newsletter.js
+++ ch4/nextevents/pages/api/newsletter.js
@@ -1,12 +1,23 @@
-export default function handler(req,res){
+import { MongoClient } from 'mongodb';
 
-    if(req.method === "POST"){
+const url = 'mongodb+srv://rajeevkumarmajhi:Gamerboy7@cluster0.4sbdhxa.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+const dbName = 'newsletter';
+
+export default async function handler(req, res) {
+    
+    await client.connect();
+    const db = client.db(dbName);
+
+    if (req.method === "POST") {
         const userEmail = req.body.email;
-        if(!userEmail || !userEmail.includes('@')){
+        if (!userEmail || !userEmail.includes('@')) {
             res.status(422).json({ message: 'Invalid Email Address' });
             return;
         }
+        await db.collection('emails').insertOne({email:userEmail});
+
+        client.close();
         res.status(201).json({ message: 'Signed Up!' })
-        console.log(userEmail);
     }
 }
