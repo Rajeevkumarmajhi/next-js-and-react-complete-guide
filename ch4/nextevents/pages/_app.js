441ce0dedb44d51e2ef285e6ecd39ef41a37c430
Rajeevn Majhi <rajeevkumarmajhi@gmail.com>
2023-03-05 23:29:11 +0545

    Adding notification context using notification context on newsletter

--- ch4/nextevents/pages/_app.js
+++ ch4/nextevents/pages/_app.js
@@ -1,13 +1,20 @@
 import Layout from '@/components/layout/layout'
+import { NotificationContextProvider } from '@/store/notification-context'
 import '@/styles/globals.css'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Layout>
-    <Head>
-      <title>Rajeev Kumar Majhi (Events App)</title>
-      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    </Head>
-    <Component {...pageProps} />
-  </Layout> 
+  return (
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Rajeev Kumar Majhi (Events App)</title>
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
+        <Component {...pageProps} />
+      </Layout> 
+    </NotificationContextProvider>
+  )
+  
+  
 }
